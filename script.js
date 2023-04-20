23c23004c298aa000717356926913a1a147ffdc1
Angel Maury <angelito2507v@gmail.com>
2023-04-19 20:15:09 -0500

    V0.2, Falta Limpiar el codigo js, mucho codigo repetido.

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const inputMes = document.getElementById("mes");
 const inputAño = document.getElementById("año");
 
 const textErrorDia = document.querySelector(".input-text-error-dias");
+const textErrorDias30 = document.querySelector(".input-text-error-dias30");
+const textErrorMes30 = document.querySelector(".input-text-error-mes30");
 const textErrorMes = document.querySelector(".input-text-error-meses");
 const textErrorAño = document.querySelector(".input-text-error-años");
+const textBotonError = document.querySelector('.text-error-btn');
 
 const rayitaAño = document.getElementById("rayitas_años");
 const rayitaMes = document.getElementById("rayitas_meses");
 const rayitaDia = document.getElementById("rayitas_dias");
 
 
-function validarCampo (v,t,i){
+function validarDigitos (v,t,i){
     v.addEventListener('change', () =>{
         if(inputDia.value > 31 || inputMes.value > 12 ||inputAño.value > 2023){
             v.classList.add("input-error");
@@ -19,43 +22,136 @@ function validarCampo (v,t,i){
             v.classList.toggle('shake-horizontal');
             t.classList.add('active');
             campos[v.id] = null
-        }else{
-            v.classList.remove('input-error');
-            v.classList.remove('snake-horizontal')
-            v.classList.add(i);
-            t.classList.remove('active');
-            campos[v.id] = v.value
+        }
+        else{
+            if (inputMes.value == 4 && inputDia.value > 30) {
+                v.classList.add("input-error");
+                v.classList.remove(i);
+                v.classList.toggle('shake-horizontal');
+                textErrorDias30.classList.add('active');
+                campos[v.id] = null
+            }
+            else{
+                v.classList.remove('input-error');
+                v.classList.remove('snake-horizontal');
+                v.classList.add(i);
+                textErrorDias30.classList.remove('active');
+                campos[v.id] = v.value
+                if (inputMes.value == 11 && inputDia.value > 30) {
+                    v.classList.add("input-error");
+                    v.classList.remove(i);
+                    v.classList.toggle('shake-horizontal');
+                    textErrorDias30.classList.add('active');
+                    campos[v.id] = null
+                }
+                else{
+                    v.classList.remove('input-error');
+                    v.classList.remove('snake-horizontal');
+                    v.classList.add(i);
+                    textErrorDias30.classList.remove('active');
+                    campos[v.id] = v.value
+                    if (inputMes.value == 6 && inputDia.value > 30) {
+                        v.classList.add("input-error");
+                        v.classList.remove(i);
+                        v.classList.toggle('shake-horizontal');
+                        textErrorDias30.classList.add('active');
+                        campos[v.id] = null
+                    }
+                    else{
+                        v.classList.remove('input-error');
+                        v.classList.remove('snake-horizontal');
+                        v.classList.add(i);
+                        textErrorDias30.classList.remove('active');
+                        campos[v.id] = v.value
+                        if (inputMes.value == 9 && inputDia.value > 30) {
+                            v.classList.add("input-error");
+                            v.classList.remove(i);
+                            v.classList.toggle('shake-horizontal');
+                            textErrorDias30.classList.add('active');
+                            campos[v.id] = null
+                        }
+                        else{
+                            v.classList.remove('input-error');
+                            v.classList.remove('snake-horizontal');
+                            v.classList.add(i);
+                            textErrorDias30.classList.remove('active');
+                            campos[v.id] = v.value
+                        }
+                        if (inputMes.value == 2 && inputDia.value > 28) {
+                            v.classList.add("input-error");
+                            v.classList.remove(i);
+                            v.classList.toggle('shake-horizontal');
+                            textErrorDias30.classList.add('active');
+                            campos[v.id] = null
+                        }
+                        else{
+                            v.classList.remove('input-error');
+                            v.classList.remove('snake-horizontal');
+                            v.classList.add(i);
+                            textErrorDias30.classList.remove('active');
+                            campos[v.id] = v.value
+                        }
+                    }
+                }
+            }
+
+        v.classList.remove('input-error');
+        v.classList.remove('snake-horizontal');
+        v.classList.add(i);
+        t.classList.remove('active');
+        campos[v.id] = v.value
         }
     })
 }
 
+validarDigitos (inputDia,textErrorDia, "input1");
+validarDigitos (inputMes,textErrorMes, "input2");
+validarDigitos (inputAño,textErrorAño, "input3");
+
 campos = {}
-
-validarCampo (inputDia,textErrorDia, "input1");
-validarCampo (inputMes,textErrorMes, "input2");
-validarCampo (inputAño,textErrorAño, "input3");
-
 
 const btnCalcular = document.getElementById('btnCal');
 
-btnCalcular.addEventListener('click', () =>{
-const fechaActual = new Date();
-const dia = fechaActual.getDate();
-const mes = fechaActual.getMonth() + 1;
-const anio = fechaActual.getFullYear();
-/* console.log(`${dia}/${mes}/${anio}`); */
 
-const diaIngresado = parseInt(campos.dia);
-const diferenciaDia = Math.abs(dia - diaIngresado);
-rayitaDia.innerText = diferenciaDia;
+function calcular (){
+    if(campos.dia === undefined || campos.mes === undefined || campos.año === undefined){
+        textBotonError.classList.remove('desactive');
 
-const mesIngresado = parseInt(campos.mes);
-const diferenciaMes = Math.abs(mes - mesIngresado);
-rayitaMes.innerText = diferenciaMes;
+        inputDia.classList.add('input-error');
+        inputMes.classList.add('input-error');
+        inputAño.classList.add('input-error');
 
-const añoIngresado = parseInt(campos.año);
-const diferenciaAño = Math.abs(anio - añoIngresado);
-rayitaAño.innerText = diferenciaAño;
-})
+        inputDia.classList.remove("input1");
+        inputMes.classList.remove("input2");
+        inputAño.classList.remove("input3");
 
+        inputDia.classList.toggle('shake-horizontal');
+        inputMes.classList.toggle('shake-horizontal');
+        inputAño.classList.toggle('shake-horizontal');
+        return;
+    }else{
+        textBotonError.classList.add('desactive');
+        inputDia.classList.remove('input-error');
+        inputMes.classList.remove('input-error');
+        inputAño.classList.remove('input-error');
 
+    }
+    const fechaActual = new Date();
+    const dia = fechaActual.getDate();
+    const mes = fechaActual.getMonth() + 1;
+    const anio = fechaActual.getFullYear();
+    
+    const diaIngresado = parseInt(campos.dia);
+    const diferenciaDia = Math.abs(diaIngresado - dia);
+    rayitaDia.innerText = diferenciaDia;
+    
+    const mesIngresado = parseInt(campos.mes);
+    const diferenciaMes = Math.abs(mesIngresado - mes);
+    rayitaMes.innerText = diferenciaMes;
+    
+    const añoIngresado = parseInt(campos.año);
+    const diferenciaAño = Math.abs(añoIngresado - anio);
+    rayitaAño.innerText = diferenciaAño;
+}
+
+btnCalcular.addEventListener('click', calcular)
